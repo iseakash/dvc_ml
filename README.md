5a29dc31e17434f6d701aa57d6d16d01cffd9b55
iseakash <iseakash@gmail.com>
2021-11-08 17:26:18 +0530

    commited

--- README.md
+++ README.md
@@ -124,7 +124,7 @@ stages:
 
 ## STEP 11: run the dvc repro command
 ```bash
-dvc repo
+dvc repro
 ```
 
 ## STEP 12: push the changes to remote repository
--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -27,10 +27,8 @@ def get_data(config_path):
 
 
 if __name__ == '__main__':
+    
     args = argparse.ArgumentParser()
-
     args.add_argument("--config", "-c", default="config/config.yaml")
-
     parsed_args = args.parse_args()
-
     get_data(config_path=parsed_args.config)
